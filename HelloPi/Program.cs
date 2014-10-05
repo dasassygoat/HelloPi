0174dec40afa6675e12170be2dbde224f4724b93
Bryan Pritchett <bryan@anastasisdev.com>
2014-10-05 02:55:22 -0400

    Initial Commit

---
+++ HelloPi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Raspberry.IO.GeneralPurpose;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var led1 = ConnectorPin.P1Pin11.Output();
+            var connection = new GpioConnection(led1);
+            for (var i = 0; i < 100; i++)
+            {
+                connection.Toggle(led1);
+                System.Threading.Thread.Sleep(250);
+            }
+            connection.Close();
+        }
+    }
+}
